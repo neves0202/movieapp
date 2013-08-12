2a73710cd268dd834d22e1b34891f59bd0460c90
Neves <neves0202@gmail.com>
2013-08-12 12:26:55 -0400

    adding updated code

--- rails/geektherapy/app/controllers/articles_controller.rb
+++ rails/geektherapy/app/controllers/articles_controller.rb
@@ -1,25 +1,63 @@
 class ArticlesController < ApplicationController
-	
+	before_action :load_article, :only => [:edit, :update, :show, :destroy]
+
+
 	def index 
 		@articles = Article.search_for(params[:query])
 	end
+
+	def show
+		if params[:destroy]
+    		render 'index' and return
+		end
+  	end
+
 
 	def new
 		@article = Article.new
 	end
 	
-	def show
+	def edit
+	end
+
+	def update
+		
+		if @article.update article_params
+		 	redirect_to @article
+				else
+			render 'edit'
+		end
 	end
 
 	def create
 		@article = Article.new(article_params)
-
+		
 		if @article.save
 			redirect_to @article
 		else
 			render 'new'
 		end
 	end
+
+	def destroy	
+		@article.destroy
+		redirect_to :action => :index
+
+    end
+ 
+
+
+private
+
+def load_article
+
+	@article = Article.find(params[:id])
+end
+
+def article_params
+	params.require('article').permit(:questions, :description, :category)
+end
+
 
 end
 
--- rails/geektherapy/app/models/article.rb
+++ rails/geektherapy/app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
 	
 	validates :questions, :description, :category, presence:true
-	validates :description, length: { minimum: 10}
+	#validates :description, length: { minimum: 10}
 
 	def self.search_for(query)
 		Article.where("questions LIKE :query OR description LIKE :query", query: "%#{query}%")
